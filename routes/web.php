c191c39f19136b3c4b0534fae3ce2315fa432eda
Youness Dominique <youness.dominique@gmail.com>
2024-07-29 16:26:22 +0200

    feat: update patrouille route

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,6 @@ Route::get('/', function () {
 });
 
 Route::post('api/alertes', [AlerteController::class, 'store']);
-// Dans routes/api.php
 
-Route::post('api/patrouille/login', [AlerteController::class, 'checkPatrouille']);
-
+Route::post('/patrouille/login', [AlerteController::class, 'checkPatrouille']);
 
